692e525fc59f1761b6d385dd61b7fa3c30e15da1
ahmtdigital <bilgi@atydijital.com>
2025-04-14 00:20:38 +0300

    buildadmin5

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        ignite: {
+          DEFAULT: "#E11D48",
+          700: "#BE123C",
+        },
+        dark: {
+          DEFAULT: "#121212",
+          300: "#404040",
+          400: "#262626",
+          500: "#171717",
+          600: "#0F0F0F",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
+  },
+}
